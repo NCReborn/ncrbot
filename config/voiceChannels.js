c872b80251a26162bfd5183f92d44f2476721003
mquiny <elliottrhodes@hotmail.com>
2025-09-06 22:11:43 +0100

    Add voiceChannels.js configuration file

---
+++ config/voiceChannels.js
@@ -0,0 +1,7 @@
+module.exports = {
+  collectionChannelId: 'VOICE_CHANNEL_ID_FOR_COLLECTION_VERSION', // e.g. '123456789012345678'
+  statusChannelId: 'VOICE_CHANNEL_ID_FOR_STATUS', // e.g. '987654321098765432'
+  collectionVersionPrefix: '🔵┃Collection Version: 2.3',
+  statusStable: '🟢┃Status : Stable (Latest)',
+  statusChecking: '🟣┃Status : Checking (Latest)'
+};
