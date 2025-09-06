92053db71236165f69b4f62e73878839875ae5b1
mquiny <elliottrhodes@hotmail.com>
2025-09-06 16:41:15 +0100

    Integrate crash log monitoring on bot startup
    Added crash log monitoring functionality to the bot.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const { handleVersionCommand } = require('./commands/version');
 const { handleDiffCommand } = require('./commands/diff');
+const { startCrashLogMonitor } = require('./services/crashLogService');
 const { BOT_TOKEN } = require('./config/constants');
 
 const client = new Client({
@@ -14,16 +15,17 @@ const client = new Client({
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
+  startCrashLogMonitor(client);
 });
 
 client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
-  
+
   if (message.content.startsWith('!version')) {
     await handleVersionCommand(message);
     return;
   }
-  
+
   if (message.content.startsWith('!diff')) {
     await handleDiffCommand(message);
     return;
