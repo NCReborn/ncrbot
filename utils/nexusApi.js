22ae4db01b2cac9015df6f98cb3d0f37c43e5531
mquiny <elliottrhodes@hotmail.com>
2025-09-06 20:05:47 +0100

    Refactor fetchRevision and add utility functions

--- utils/nexusApi.js
+++ utils/nexusApi.js
@@ -1,62 +1,139 @@
 const axios = require('axios');
-const { API_URL, API_KEY, APP_NAME, APP_VERSION } = require('../config/constants');
+const { COLLECTION_MAPPINGS } = require('../config/constants');
 
-module.exports = {
-  fetchRevision: async (slug, revision) => {
-    const query = `
-      query Revision($slug: String!, $revision: Int) {
-        collectionRevision(slug: $slug, revision: $revision, viewAdultContent: true) {
-          revisionNumber
-          modFiles {
+const API_URL = 'https://api-router.nexusmods.com/graphql';
+
+async function fetchRevision(slug, revision, apiKey, appName, appVersion) {
+  const query = `
+    query Revision($slug: String!, $revision: Int) {
+      collectionRevision(slug: $slug, revision: $revision, viewAdultContent: true) {
+        revisionNumber
+        modFiles {
+          fileId
+          optional
+          file {
             fileId
-            optional
-            file {
-              fileId
+            name
+            version
+            mod {
+              modId
               name
-              version
-              mod {
-                modId
+              game {
                 name
-                game {
-                  name
-                  domainName
-                }
+                domainName
               }
             }
           }
         }
       }
-    `;
-    
-    const variables = { slug, revision };
-    const headers = {
-      'Content-Type': 'application/json',
-      apikey: API_KEY,
-      'Application-Name': APP_NAME,
-      'Application-Version': APP_VERSION,
-    };
+    }
+  `;
+  
+  const variables = { slug, revision };
+  const headers = {
+    'Content-Type': 'application/json',
+    apikey: apiKey,
+    'Application-Name': appName,
+    'Application-Version': appVersion,
+  };
 
-    try {
-      const response = await axios.post(API_URL, { query, variables }, { headers, timeout: 10000 });
-      
-      if (response.data.errors) {
-        const errorMessage = response.data.errors.map(error => error.message).join(', ');
-        throw new Error(`API Error: ${errorMessage}`);
-      }
-      
-      if (!response.data.data?.collectionRevision) {
-        throw new Error(`Revision ${revision} not found for collection ${slug}`);
-      }
-      
-      return response.data.data.collectionRevision;
-    } catch (error) {
-      if (error.response) {
-        throw new Error(`API returned status ${error.response.status}: ${error.response.statusText}`);
-      } else if (error.request) {
-        throw new Error('No response received from Nexus Mods API. Please try again later.');
-      } else {
-        throw new Error(`Failed to fetch revision: ${error.message}`);
+  try {
+    const response = await axios.post(API_URL, { query, variables }, { headers, timeout: 10000 });
+    
+    if (response.data.errors) {
+      const errorMessage = response.data.errors.map(error => error.message).join(', ');
+      throw new Error(`API Error: ${errorMessage}`);
+    }
+    
+    if (!response.data.data || !response.data.data.collectionRevision) {
+      throw new Error(`Revision ${revision} not found for collection ${slug}`);
+    }
+    
+    return response.data.data.collectionRevision;
+  } catch (error) {
+    if (error.response) {
+      throw new Error(`API returned status ${error.response.status}: ${error.response.statusText}`);
+    } else if (error.request) {
+      throw new Error('No response received from Nexus Mods API. Please try again later.');
+    } else {
+      throw new Error(`Failed to fetch revision: ${error.message}`);
+    }
+  }
+}
+
+function getCollectionSlug(name) {
+  const normalizedName = name.toLowerCase().trim();
+  return COLLECTION_MAPPINGS.slugs[normalizedName] || name;
+}
+
+function getCollectionName(slug) {
+  return COLLECTION_MAPPINGS.names[slug] || slug;
+}
+
+function computeDiff(oldMods, newMods) {
+  const oldMap = new Map(oldMods.map((m) => [String(m.id), m]));
+  const newMap = new Map(newMods.map((m) => [String(m.id), m]));
+
+  const added = [];
+  const removed = [];
+  const updated = [];
+
+  for (const [id, mod] of newMap.entries()) {
+    if (!oldMap.has(id)) {
+      added.push(mod);
+    } else {
+      const oldMod = oldMap.get(id);
+      if (oldMod.version !== mod.version) {
+        updated.push({ before: oldMod, after: mod });
       }
     }
   }
+
+  for (const [id, mod] of oldMap.entries()) {
+    if (!newMap.has(id)) {
+      removed.push(mod);
+    }
+  }
+
+  return { added, removed, updated };
+}
+
+function findExclusiveChanges(diffs1, diffs2) {
+  const exclusiveAdded1 = diffs1.added.filter(mod1 => !diffs2.added.some(mod2 => mod2.id === mod1.id));
+  const exclusiveRemoved1 = diffs1.removed.filter(mod1 => !diffs2.removed.some(mod2 => mod2.id === mod1.id));
+  const exclusiveUpdated1 = diffs1.updated.filter(update1 => !diffs2.updated.some(update2 => update2.before.id === update1.before.id));
+
+  const exclusiveAdded2 = diffs2.added.filter(mod2 => !diffs1.added.some(mod1 => mod1.id === mod2.id));
+  const exclusiveRemoved2 = diffs2.removed.filter(mod2 => !diffs1.removed.some(mod1 => mod1.id === mod2.id));
+  const exclusiveUpdated2 = diffs2.updated.filter(update2 => !diffs1.updated.some(update1 => update1.before.id === update2.before.id));
+
+  return {
+    added1: exclusiveAdded1,
+    removed1: exclusiveRemoved1,
+    updated1: exclusiveUpdated1,
+    added2: exclusiveAdded2,
+    removed2: exclusiveRemoved2,
+    updated2: exclusiveUpdated2
+  };
+}
+
+function processModFiles(modFiles) {
+  return modFiles
+    .filter((mf) => mf.file && mf.file.mod)
+    .map((mf) => ({
+      id: `${mf.file.mod.modId}-${mf.file.mod.game.domainName}`,
+      name: mf.file.mod.name,
+      version: mf.file.version,
+      domainName: mf.file.mod.game.domainName,
+      modId: mf.file.mod.modId
+    }));
+}
+
+module.exports = {
+  fetchRevision,
+  getCollectionSlug,
+  getCollectionName,
+  computeDiff,
+  findExclusiveChanges,
+  processModFiles
 };
