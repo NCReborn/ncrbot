155b99e8d7407fb8e2058c56cd64429d64560ed8
mquiny <elliottrhodes@hotmail.com>
2025-09-07 12:35:41 +0100

    Refactor revisionStore to use async functions

--- utils/revisionStore.js
+++ utils/revisionStore.js
@@ -1,38 +1,39 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const logger = require('./logger');
 const DATA_FILE = path.join(__dirname, '../data/collection_state.json');
 
-function loadState() {
+async function loadState() {
   try {
-    return JSON.parse(fs.readFileSync(DATA_FILE, 'utf-8'));
+    const data = await fs.readFile(DATA_FILE, 'utf-8');
+    return JSON.parse(data);
   } catch (err) {
     logger.error(`Failed to load collection state from ${DATA_FILE}: ${err.message}`);
     return { lastRevision: null, revertAt: null };
   }
 }
 
-function saveState(state) {
+async function saveState(state) {
   try {
-    fs.writeFileSync(DATA_FILE, JSON.stringify(state, null, 2), 'utf-8');
+    await fs.writeFile(DATA_FILE, JSON.stringify(state, null, 2), 'utf-8');
   } catch (err) {
     logger.error(`Failed to save collection state to ${DATA_FILE}: ${err.message}`);
   }
 }
 
-function setRevision(revision, revertAt = null) {
-  const state = loadState();
+async function setRevision(revision, revertAt = null) {
+  const state = await loadState();
   state.lastRevision = revision;
   state.revertAt = revertAt;
-  saveState(state);
+  await saveState(state);
 }
 
-function getRevision() {
-  return loadState().lastRevision;
+async function getRevision() {
+  return (await loadState()).lastRevision;
 }
 
-function getRevertAt() {
-  return loadState().revertAt;
+async function getRevertAt() {
+  return (await loadState()).revertAt;
 }
 
 module.exports = { setRevision, getRevision, getRevertAt, loadState, saveState };
