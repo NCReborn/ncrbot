60942b30b86e7d1fa369ca99885522464e5b952e
mquiny <elliottrhodes@hotmail.com>
2025-09-07 13:17:15 +0100

    Refactor cooldown management to use consistent keys

--- utils/cooldownManager.js
+++ utils/cooldownManager.js
@@ -1,9 +1,15 @@
 const logger = require('./logger');
 const versionCooldowns = new Map();
 
+// Always use the same key format for user cooldowns: "version:user:<userId>"
+function makeVersionKey(userId) {
+  return `version:user:${userId}`;
+}
+
 function checkCooldown(userId, cooldownTime) {
+  const key = makeVersionKey(userId);
   const now = Date.now();
-  const cooldownEndTime = versionCooldowns.get(userId) || 0;
+  const cooldownEndTime = versionCooldowns.get(key) || 0;
   if (now < cooldownEndTime) {
     logger.info(`[VERSION] Cooldown hit for user ${userId} (${Math.ceil((cooldownEndTime - now) / 1000 / 60)} min left)`);
     return Math.ceil((cooldownEndTime - now) / 1000 / 60);
@@ -12,7 +18,8 @@ function checkCooldown(userId, cooldownTime) {
 }
 
 function setCooldown(userId, cooldownTime) {
-  versionCooldowns.set(userId, Date.now() + cooldownTime);
+  const key = makeVersionKey(userId);
+  versionCooldowns.set(key, Date.now() + cooldownTime);
   logger.info(`[VERSION] Cooldown set for user ${userId} for ${cooldownTime / 60000} minutes`);
 }
 
@@ -21,7 +28,7 @@ function cleanupOldCooldowns() {
   for (const [key, endTime] of versionCooldowns.entries()) {
     if (currentTime > endTime) {
       versionCooldowns.delete(key);
-      logger.info(`[VERSION] Cooldown expired for user ${key}`);
+      logger.info(`[VERSION] Cooldown expired for ${key}`);
     }
   }
 }
