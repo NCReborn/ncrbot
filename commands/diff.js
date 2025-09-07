4a06ecf12aad57b90e59a9d275cea4f191f48dd5
mquiny <elliottrhodes@hotmail.com>
2025-09-07 13:38:47 +0100

    Update diff command description for admin use

--- commands/diff.js
+++ commands/diff.js
@@ -12,9 +12,9 @@ const USER_COOLDOWN = 5 * 60 * 1000; // 5 minutes
 const GLOBAL_COOLDOWN = 30 * 1000;   // 30 seconds
 
 module.exports = {
-  data: new SlashCommandBuilder()
+   data: new SlashCommandBuilder()
     .setName('diff')
-    .setDescription('Show mod differences between collection revisions')
+    .setDescription('Show mod differences between collection revisions (Admin only)')
     .addStringOption(option =>
       option.setName('collection1').setDescription('First collection name (e.g. NCR, ADR)').setRequired(true))
     .addIntegerOption(option =>
