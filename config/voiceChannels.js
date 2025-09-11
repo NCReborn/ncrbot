f208405a597418e9bf459e427d6c9519fa01923a
mquiny <elliottrhodes@hotmail.com>
2025-09-11 21:45:04 +0100

    Update collection version prefix to 2.31

--- config/voiceChannels.js
+++ config/voiceChannels.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectionChannelId: '1395500774150111422', // e.g. '123456789012345678'
   statusChannelId: '1395501617523986644', // e.g. '987654321098765432'
-  collectionVersionPrefix: '🔵┃Collection Version: 2.3',
+  collectionVersionPrefix: '🔵┃Collection Version: 2.31',
   statusStable: '🟢┃Status : Stable (Latest)',
   statusChecking: '🟣┃Status : Checking (Latest)'
 };
