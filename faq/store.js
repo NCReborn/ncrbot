a5aea52530888646a061e4e0809fa5a2a370e2c0
mquiny <elliottrhodes@hotmail.com>
2025-09-20 00:51:32 +0100

    Update FAQ loading mechanism and remove unused functions

--- faq/store.js
+++ faq/store.js
@@ -1,29 +1,10 @@
-// Stores all FAQ entries in memory and persists to disk (optional).
-
 const fs = require('fs');
 const path = require('path');
 
-const FAQ_PATH = path.join(__dirname, 'faqs.json');
-
-let faqs = [];
-
 function loadFAQs() {
-    if (fs.existsSync(FAQ_PATH)) {
-        faqs = JSON.parse(fs.readFileSync(FAQ_PATH, 'utf8'));
-    }
+  const faqsPath = path.join(__dirname, 'faq.json');
+  if (!fs.existsSync(faqsPath)) return [];
+  return JSON.parse(fs.readFileSync(faqsPath, 'utf8'));
 }
 
-function saveFAQs() {
-    fs.writeFileSync(FAQ_PATH, JSON.stringify(faqs, null, 2), 'utf8');
-}
-
-function setFAQs(newFAQs) {
-    faqs = newFAQs;
-    saveFAQs();
-}
-
-function getFAQs() {
-    return faqs;
-}
-
-module.exports = { loadFAQs, saveFAQs, setFAQs, getFAQs };
+module.exports = { loadFAQs };
