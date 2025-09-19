70da4460ac1363f9bbd45e7747bb88aac1f56938
mquiny <elliottrhodes@hotmail.com>
2025-09-19 02:26:14 +0100

    Update group members and collections in config

--- config/collections.js
+++ config/collections.js
@@ -6,19 +6,19 @@ module.exports = {
     {
       name: 'FULL',
       channelId: process.env.FULL_GROUP_CHANNEL_ID || '1285797113879334962',
-      members: ['ncr', 'adr']
+      members: ['rcuccp', 'srpv39']
     },
     {
       name: 'LITE',
       channelId: process.env.LITE_GROUP_CHANNEL_ID || '1387411802035585086',
-      members: ['ncrlite', 'adrlite']
+      members: ['vfy7w1', 'ezxduq']
     }
   ],
   // Master list of tracked collections.
   collections: [
-    { slug: 'ncr', display: 'NCR' },
-    { slug: 'adr', display: 'ADR' },
-    { slug: 'ncrlite', display: 'NCR Lite' },
-    { slug: 'adrlite', display: 'ADR Lite' }
+    { slug: 'rcuccp', display: 'NCR' },
+    { slug: 'srpv39', display: 'ADR' },
+    { slug: 'vfy7w1', display: 'NCR Lite' },
+    { slug: 'ezxduq', display: 'ADR Lite' }
   ]
 };
