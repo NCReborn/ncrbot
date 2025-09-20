916b8c3ce79945975069a414c1333c6f1f5c888f
mquiny <elliottrhodes@hotmail.com>
2025-09-20 12:33:55 +0100

    Add ready event handler for client login

---
+++ events/ready.js
@@ -0,0 +1,24 @@
+const logger = require('../utils/logger');
+const { sendLogScanButton } = require('../utils/logScanTicket');
+
+module.exports = {
+  name: 'ready',
+  once: true,
+  async execute(client) {
+    logger.info(`Logged in as ${client.user.tag}`);
+    logger.info(`Loaded ${client.commands.size} commands.`);
+    logger.info(`Crash log channel: ${process.env.CRASH_LOG_CHANNEL_ID}, Log scan channel: ${process.env.LOG_SCAN_CHANNEL_ID}`);
+    logger.info(`Revision polling enabled: ${!!process.env.NEXUS_API_KEY}`);
+    client.user.setActivity('/help for commands', { type: 'LISTENING' });
+
+    // Send log scan button (as before)
+    try {
+      await sendLogScanButton(client, process.env.LOG_SCAN_CHANNEL_ID);
+    } catch (err) {
+      logger.error(`Error sending log scan button: ${err.stack || err}`);
+    }
+
+    // ...move the revision polling logic here as a helper function or another util.
+    require('../utils/revisionPoller').start(client);
+  }
+};
