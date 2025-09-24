e442ff5aea158a96e2c0de60c5f04b9708ecf763
mquiny <elliottrhodes@hotmail.com>
2025-09-24 22:54:43 +0100

    Refactor command data structure using SlashCommandBuilder

--- commands/listmediachannels.js
+++ commands/listmediachannels.js
@@ -1,10 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
 const config = require('../config/imageOnlyConfig.json');
 
 module.exports = {
-  data: {
-    name: 'listmediachannels',
-    description: 'List all image-only and file-only channels.'
-  },
+  data: new SlashCommandBuilder()
+    .setName('listmediachannels')
+    .setDescription('List all image-only and file-only channels.'),
   async execute(interaction) {
     const imageChannels = config.imageOnlyChannels.map(id => `<#${id}>`).join('\n') || 'None set';
     const fileChannels = config.fileOnlyChannels.map(id => `<#${id}>`).join('\n') || 'None set';
