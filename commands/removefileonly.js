71407b9925ded564d30030aeba99050c4bf54762
mquiny <elliottrhodes@hotmail.com>
2025-09-24 22:51:15 +0100

    Add removefileonly command to manage file-only channels

---
+++ commands/removefileonly.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const configPath = path.join(__dirname, '../config/imageOnlyConfig.json');
+
+module.exports = {
+  data: {
+    name: 'removefileonly',
+    description: 'Remove a channel from file-only enforcement.',
+    options: [{
+      name: 'channel',
+      description: 'Channel to remove',
+      type: 7, // CHANNEL type
+      required: true,
+    }]
+  },
+  async execute(interaction) {
+    if (!interaction.member.permissions.has('ManageGuild')) {
+      return interaction.reply({ content: 'You need Manage Server permissions.', ephemeral: true });
+    }
+    const channel = interaction.options.getChannel('channel');
+    let config = require(configPath);
+
+    if (config.fileOnlyChannels.includes(channel.id)) {
+      config.fileOnlyChannels = config.fileOnlyChannels.filter(id => id !== channel.id);
+      fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
+      return interaction.reply(`Channel <#${channel.id}> removed from file-only list.`);
+    } else {
+      return interaction.reply(`Channel <#${channel.id}> is not in the file-only list.`);
+    }
+  }
+};
