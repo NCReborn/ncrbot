22db4fbbb1fdf0ad292a5ed9291fe0365ed2b1d2
mquiny <elliottrhodes@hotmail.com>
2025-09-24 22:54:54 +0100

    Rename command to remove image-only enforcement

--- commands/removefileonly.js
+++ commands/removefileonly.js
@@ -1,18 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 const configPath = path.join(__dirname, '../config/imageOnlyConfig.json');
 
 module.exports = {
-  data: {
-    name: 'removefileonly',
-    description: 'Remove a channel from file-only enforcement.',
-    options: [{
-      name: 'channel',
-      description: 'Channel to remove',
-      type: 7, // CHANNEL type
-      required: true,
-    }]
-  },
+  data: new SlashCommandBuilder()
+    .setName('removeimageonly')
+    .setDescription('Remove a channel from image-only enforcement.')
+    .addChannelOption(option =>
+      option.setName('channel')
+        .setDescription('Channel to remove')
+        .setRequired(true)
+    ),
   async execute(interaction) {
     if (!interaction.member.permissions.has('ManageGuild')) {
       return interaction.reply({ content: 'You need Manage Server permissions.', ephemeral: true });
@@ -20,12 +19,12 @@ module.exports = {
     const channel = interaction.options.getChannel('channel');
     let config = require(configPath);
 
-    if (config.fileOnlyChannels.includes(channel.id)) {
-      config.fileOnlyChannels = config.fileOnlyChannels.filter(id => id !== channel.id);
+    if (config.imageOnlyChannels.includes(channel.id)) {
+      config.imageOnlyChannels = config.imageOnlyChannels.filter(id => id !== channel.id);
       fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
-      return interaction.reply(`Channel <#${channel.id}> removed from file-only list.`);
+      return interaction.reply(`Channel <#${channel.id}> removed from image-only list.`);
     } else {
-      return interaction.reply(`Channel <#${channel.id}> is not in the file-only list.`);
+      return interaction.reply(`Channel <#${channel.id}> is not in the image-only list.`);
     }
   }
 };
