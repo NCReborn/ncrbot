e9af307753d1a66be4d0ada93b8ca4aba6da01af
mquiny <elliottrhodes@hotmail.com>
2025-09-26 22:49:27 +0100

    Update guild ID in clearCommands.js

--- utils/clearCommands.js
+++ utils/clearCommands.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   await client.application.commands.set([]); // Removes all global commands
   // For guild-specific commands (uncomment and set your guild ID):
-  // const guild = client.guilds.cache.get('YOUR_GUILD_ID');
+  const guild = client.guilds.cache.get('1285796904160202752');
   // await guild.commands.set([]); // Removes all guild commands
 };
