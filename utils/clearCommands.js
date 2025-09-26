33807deec57b505571469448d9b3a3d9424afac7
mquiny <elliottrhodes@hotmail.com>
2025-09-26 23:07:08 +0100

    Enhance command clearing to handle all guilds
    Updated command clearing logic to remove guild commands for all guilds.

--- utils/clearCommands.js
+++ utils/clearCommands.js
@@ -1,6 +1,8 @@
 module.exports = async (client) => {
-  await client.application.commands.set([]); // Removes all global commands
-  // For guild-specific commands (uncomment and set your guild ID):
-  const guild = client.guilds.cache.get('1285796904160202752');
-  // await guild.commands.set([]); // Removes all guild commands
+  // Remove global commands
+  await client.application.commands.set([]);
+  // Remove guild commands for all guilds the bot is in
+  for (const [guildId, guild] of client.guilds.cache) {
+    await guild.commands.set([]);
+  }
 };
