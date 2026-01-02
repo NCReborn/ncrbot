10c01846e6c2226f4c0e7fc1d6d4f2e084fee454
mquiny <elliottrhodes@hotmail.com>
2026-01-02 22:04:53 +0000

    Update unblocksupportmention.js

--- commands/unblocksupportmention.js
+++ commands/unblocksupportmention.js
@@ -1,9 +1,9 @@
-import { SlashCommandBuilder } from 'discord.js';
-import { hasModRole } from '../utils/hasModRole.js';
+const { SlashCommandBuilder } = require('discord.js');
+const { hasModRole } = require('../utils/hasModRole.js');
 
 const PING_BANNED_ROLE_ID = '1456763426159329555';
 
-export default {
+module.exports = {
     data: new SlashCommandBuilder()
         .setName('unblocksupportmention')
         .setDescription('Allow a user to mention the support role again by removing the Ping Banned role')
@@ -14,16 +14,24 @@ export default {
         ),
     async execute(interaction) {
         if (!hasModRole(interaction.member)) {
-            return await interaction.reply({ content: "You don't have permission to use this command.", ephemeral: true });
+            return interaction.reply({ content: "You don't have permission to use this command.", ephemeral: true });
         }
         const user = interaction.options.getUser('user');
         const member = await interaction.guild.members.fetch(user.id);
 
         if (!member.roles.cache.has(PING_BANNED_ROLE_ID)) {
-            return await interaction.reply({ content: `${user.tag} is not ping-banned.`, ephemeral: true });
+            return interaction.reply({ content: `${user.tag} is not ping-banned.`, ephemeral: true });
         }
 
         await member.roles.remove(PING_BANNED_ROLE_ID, 'Unblocked from mentioning support role');
+
+        // Attempt to DM the user
+        try {
+            await user.send(
+                `You have been unbanned from mentioning the support role in **${interaction.guild.name}**. Please use this responsibly!`
+            );
+        } catch (err) { /* ignore if user has DMs blocked */ }
+
         await interaction.reply({ content: `${user.tag} can now mention the support role again.`, ephemeral: true });
-    }
+    },
 };
