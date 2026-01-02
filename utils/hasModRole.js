22560f9efe8fc5cc2080e1a214ab467502aeb62c
mquiny <elliottrhodes@hotmail.com>
2026-01-02 21:48:13 +0000

    Create hasModRole utility function
    Adds a utility function to check if a member has moderator role or admin permissions.

---
+++ utils/hasModRole.js
@@ -0,0 +1,6 @@
+// Returns true if the user is an administrator or has the Mod+ role
+const MOD_ROLE_ID = '1370874936456908931';
+
+export function hasModRole(member) {
+    return member.permissions.has('Administrator') || member.roles.cache.has(MOD_ROLE_ID);
+}
