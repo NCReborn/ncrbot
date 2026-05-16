8498ff549bb878e44483a34717400c1819c2a268
mquiny <elliottrhodes@hotmail.com>
2026-05-16 20:28:20 +0100

    Refactor changelog command to use interactions

--- commands/changelog_v2.js
+++ commands/changelog_v2.js
@@ -1,32 +1,45 @@
-const fs = require("fs");
-const path = require("path");
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 const buildChangelogEmbed = require("../utils/changelogBuilder_v2");
 
-
 module.exports = {
-    name: "changelogv2",
-    description: "Post the new modular changelog",
-    async execute(message, args, client) {
+    data: new SlashCommandBuilder()
+        .setName("changelogv2")
+        .setDescription("Post the new modular changelog (Admin only)")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+    async execute(interaction) {
 
         const changelogChannel = "1285797113879334962";
 
         const oldData = {
-            core: require("../../data/collections/core_old.json"),
-            extras: require("../../data/collections/extras_old.json"),
-            body: require("../../data/collections/body_old.json")
+            core: require("../data/collections/core_old.json"),
+            extras: require("../data/collections/extras_old.json"),
+            body: require("../data/collections/body_old.json")
         };
 
         const newData = {
-            core: require("../../data/collections/core.json"),
-            extras: require("../../data/collections/extras.json"),
-            body: require("../../data/collections/body.json")
+            core: require("../data/collections/core.json"),
+            extras: require("../data/collections/extras.json"),
+            body: require("../data/collections/body.json")
         };
 
         const embed = buildChangelogEmbed(oldData, newData);
 
-        const channel = await client.channels.fetch(changelogChannel);
-        await channel.send({ embeds: [embed] });
+        try {
+            const channel = await interaction.client.channels.fetch(changelogChannel);
+            await channel.send({ embeds: [embed] });
 
-        message.reply("Changelog posted successfully.");
+            return interaction.reply({
+                content: "Changelog posted successfully.",
+                ephemeral: true
+            });
+
+        } catch (err) {
+            console.error(err);
+            return interaction.reply({
+                content: "Failed to post changelog.",
+                ephemeral: true
+            });
+        }
     }
 };
