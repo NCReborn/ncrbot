a596d1b75bd4104d23edcc0bbe556e23a91b9868
mquiny <elliottrhodes@hotmail.com>
2026-05-16 20:28:30 +0100

    Refactor testchangelog command to use interactions

--- commands/testchangelog.js
+++ commands/testchangelog.js
@@ -1,25 +1,42 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 const buildChangelogEmbed = require("../utils/changelogBuilder_v2");
 
 module.exports = {
-    name: "testchangelog",
-    description: "Preview the new modular changelog without posting it",
-    async execute(message, args, client) {
+    data: new SlashCommandBuilder()
+        .setName("testchangelog")
+        .setDescription("Preview the new modular changelog in DMs (Admin only)")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+    async execute(interaction) {
 
         const oldData = {
-            core: require("../../data/collections/core_old.json"),
-            extras: require("../../data/collections/extras_old.json"),
-            body: require("../../data/collections/body_old.json")
+            core: require("../data/collections/core_old.json"),
+            extras: require("../data/collections/extras_old.json"),
+            body: require("../data/collections/body_old.json")
         };
 
         const newData = {
-            core: require("../../data/collections/core.json"),
-            extras: require("../../data/collections/extras.json"),
-            body: require("../../data/collections/body.json")
+            core: require("../data/collections/core.json"),
+            extras: require("../data/collections/extras.json"),
+            body: require("../data/collections/body.json")
         };
 
         const embed = buildChangelogEmbed(oldData, newData);
 
-        message.author.send({ embeds: [embed] })
-            .catch(() => message.reply("I couldn't DM you the preview."));
+        try {
+            await interaction.user.send({ embeds: [embed] });
+
+            return interaction.reply({
+                content: "Sent you a DM with the preview.",
+                ephemeral: true
+            });
+
+        } catch (err) {
+            console.error(err);
+            return interaction.reply({
+                content: "I couldn't DM you the preview.",
+                ephemeral: true
+            });
+        }
     }
 };
