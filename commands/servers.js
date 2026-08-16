06ca13f496620c1e8a70fe8092092ec38f509406
mquiny <elliottrhodes@hotmail.com>
2026-08-16 02:35:19 +0100

    Update and rename ncrebornservers.js to servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -2,11 +2,13 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('NCReborn-servers')
+        .setName('servers')
         .setDescription('List all servers the bot is currently in'),
 
     async execute(interaction) {
-        const guilds = interaction.client.guilds.cache.map(g => `${g.name} (${g.id})`);
+        const guilds = interaction.client.guilds.cache.map(
+            g => `${g.name} (${g.id})`
+        );
 
         await interaction.reply({
             content: `**Servers I'm currently in:**\n${guilds.join('\n')}`,
