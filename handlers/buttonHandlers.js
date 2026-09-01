74b1da1a664aec9036e52bf7fb6cbec054774edf
mquiny <elliottrhodes@hotmail.com>
2026-09-01 20:31:01 +0100

    Update import path for handleLeaderboardButton

--- handlers/buttonHandlers.js
+++ handlers/buttonHandlers.js
@@ -1,7 +1,7 @@
 const logger = require('../utils/logger');
 const { PermissionFlagsBits, EmbedBuilder } = require('discord.js');
 const SpamActionHandler = require('../services/spam/SpamActionHandler');
-const { handleLeaderboardButton } = require('../commands/streetcred/streetcred.js');
+const { handleLeaderboardButton } = require('../commands/utility/streetcred.js');
 
 class ButtonHandlers {
   async handle(interaction, client) {
